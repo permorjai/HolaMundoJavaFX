5cc5f2b49f3e5c433ee63e479f4b049ef8d77e5d
permorjai <jaime@cb.cbtis182.edu.mx>
2025-10-16 11:53:33 -0600

    Tercer commit

--- src/main/java/com/ejemplojavafx/holamundojavafx/HelloApplication.java
+++ src/main/java/com/ejemplojavafx/holamundojavafx/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("¡Hola Mundo desde JavaFX! - Jaime, Danna, Jocelyn");
         stage.setScene(scene);
         stage.show();
     }
